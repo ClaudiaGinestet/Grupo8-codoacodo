503c8865c55895531cd0c2e3c505df4449a1f144
Jimmy Martinez <jimmymartinez@outlook.com>
2024-04-22 22:26:13 -0300

    validacion error con javascript inicial

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 var fecha=new Date();  
 var anio=" - "+fecha.getUTCFullYear();  
 document.getElementById('fechafoot').innerHTML=anio; 
+
+function validar() {
+    const Nombre = document.getElementById("nombre").value;
+    const nombreError = document.getElementById("nombreError");
+    nombreError.textContent = "";
+    let esValido = true;
+
+    if (Nombre === "" || /\d/.test(Nombre)) {
+        nombreError.textContent = "Por favor ingrese su nombre.";
+        esValido = false;
+    }
+    return esValido;
+}
